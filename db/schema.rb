f01f0c8b612963a61447ea9a18fb27062bbaa215
Anderson Nunes <anderson.nunes1988@gmail.com>
2017-07-29 19:34:11 -0300

    Adding Articles CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170729214303) do
+ActiveRecord::Schema.define(version: 20170729221047) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "slug"
+    t.text "body"
+    t.string "description"
+    t.integer "favorites_count"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["slug"], name: "index_articles_on_slug", unique: true
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
